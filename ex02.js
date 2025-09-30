fdbbb5658ef64a3c611ac3d2c74aefb2ea8d3d0d
Diana Laura Gonzalez Escamilla <dianathcb@gmail.com>
2025-09-29 18:26:00 -0700

    ex02 completed

--- ex02.js
+++ ex02.js
@@ -2,4 +2,8 @@
 // containing the elements that are in the first array but not in the second array.
 // Example: arrayDifference([1, 2, 3], [2, 3, 4]) should return [1].
 
+const arrayDifference = function (numsA, numsB) {
+    return numsA.filter(num => !numsB.includes(num))
+}
+
 console.log(arrayDifference([1, 2, 3], [2, 3, 4])); // Expected output: [1]
