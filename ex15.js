e80b8c83d1f9efa85aa8787809e67fc3c8447755
Diana Laura Gonzalez Escamilla <dianathcb@gmail.com>
2025-09-29 19:50:35 -0700

    ex15 Completed

--- ex15.js
+++ ex15.js
@@ -2,4 +2,10 @@
 // and returns the maximum number in the array.
 // Example: findMax([3, 1, 4, 1, 5, 9]) should return 9.
 
+// const max = num.toSorted((a, b) => b - a)
+//     return max [0]
+
+const findMax = function (num) {
+    return num.toSorted((a, b) => b - a)[0]
+}
 console.log(findMax([3, 1, 4, 1, 5, 9])); // Expected output: 9
